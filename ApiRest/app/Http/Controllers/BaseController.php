9c0e6d76a88c1e46305afab9c6a3acda4855941d
buruhsd <buruhsd@outlook.co.id>
2022-11-10 21:36:26 +0700

    create BaseController

---
+++ ApiRest/app/Http/Controllers/BaseController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+        return response()->json($response, 200);
+    }
+
+    /**
+     * return error response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function sendError($error, $errorMessages = [], $code = 500)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+        if (!empty($errorMessages)) {
+            $response['data'] = (is_null($errorMessages))?"Shit happened, error on system please call Isa or Daus to fix this!":$errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
